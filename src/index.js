6bf10b4526abd38abc9b7d1c92834d4bd773b557
Daniel Palmieri <dpalmieri040@gmail.com>
2020-02-12 19:27:09 -0500

    added root div to enable react to render elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react=dom'
 import './index.css'
+
+// create overall card class - this will hold each attribute of the
+class GitCard extends React.Component{
+
+}
