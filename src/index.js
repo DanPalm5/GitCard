68a40f27c90eae22a17756be0d189452be6a951d
Daniel Palmieri <dpalmieri@ycp.edu>
2020-02-15 20:55:29 -0500

    updates to gitCard class
    added fields to hold different information, as well as created the API that will fetch this data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react=dom'
+import React, { Profiler } from 'react'
+import ReactDOM from 'react-dom'
 import './index.css'
 
-// create overall card class - this will hold each attribute of the
-class GitCard extends React.Component{
+// this is the API i will use to pull data 
+const API = 'https://api.github.com/users'
 
+// create overall gitcard class - this will hold each field of the card
+class GitCard extends React.Component{
+    constructor(props) {
+        super(props)
+        // these are the fields of the card
+        this.state = {
+            name: '',
+            profilePicture: '',
+            repos: '',
+            followers: '',
+            following: '',
+            location: '',
+            bio: ''
+
+        }
+    }
+    
+
+
+
+    render(
+       
+
+
+
+
+
+    )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////
+ReactDOM.render(
+    <GitCard />, 
+    document.getElementById('root'));
