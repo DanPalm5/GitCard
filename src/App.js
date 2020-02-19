5f21d5d70d6255e19bc105dcb5557478d8c7e9ec
Daniel Palmieri <dpalmieri@ycp.edu>
2020-02-19 13:38:36 -0500

    reorganized files
    created components folder, restructured app files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GitCard from './components/Gitcard'
+import './style.css'
+
+// API to get data
+const API = 'https://api.github.com/users'
+
+
+
+class App extends React.Component {
+    render(){
+    return(
+        <div className = "App">
+            <h1>G I T C A R D</h1>
+            <h2>A B O U T </h2>
+            <h2>S E A R C H </h2>
+        </div>
+    )
+    }
+}
+
+
+
+export default App;
---
+++ src/components/Gitcard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+// create overall gitcard class - this will hold each field of the card
+class GitCard extends React.Component{
+        state = {
+            name: '',
+            profilePicture: '',
+            repos: '',
+            followers: '',
+            following: '',
+            location: '',
+            
+
+        }
+    }
+
+
+export default GitCard;
